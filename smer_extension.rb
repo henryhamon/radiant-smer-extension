5dedd32d9af5fc8b8a0d24d8afb0048802161d59
Henry <henryhamon@gmail.com>
2008-09-29 20:41:48 -0300

    Fixed the project url

--- smer_extension.rb
+++ smer_extension.rb
@@ -1,14 +1,14 @@
 class SmerExtension < Radiant::Extension
   version "0.0"
   description "Simple Mailer Extension for Radiant. That's provide simple support for email forms."
-  url "http://github.com/"
+  url "http://github.com/henryhamon/radiant-smer-extension"
 
   define_routes do |map|
 #    map.resources :mail, :path_prefix => "/pages/:page_id", :controller => "mail"
   end
   
 #  def activate
-#    admin.tabs.add "MailConfig", "/admin/", :after => "Layouts", :visibility => [:all]
+#    admin.tabs.add "Mail Config", "/admin/", :after => "Layouts", :visibility => [:all]
 #  end
     
 end
